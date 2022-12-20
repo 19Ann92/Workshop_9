45cbc251e20897f7e50cec7d7a81c754d0d0fdb0
Anna Eliseeva <Panterka6868@mail.ru>
2022-12-20 13:59:28 +0300

    add Example 64

---
+++ Example_64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая 
+// выведет все чётные натуральные числа в промежутке от M до N 
+// с помощью рекурсии.
+
+// M = 1; N = 5 -> 2, 4
+// M = 4; N = 8 -> 4, 6, 8
+
+Console.Clear();
+
+Console.WriteLine("Введите число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int FindNumber(int ot, int do1)
+{
+    if (ot > do1) return 0;
+    else if (ot % 2 == 0)
+        Console.Write(ot + ", ");
+    return FindNumber(ot + 1, do1);
+}
+
+if (m > n) FindNumber(n, m);
+else FindNumber(m, n);
+
